572cae80a84de11177476fc9cd6573054c638b07
AshwinChandlapur <ashwinyga@rocketmail.com>
2016-03-07 04:12:11 +0530

    CSS reduced

--- js/jova.js
+++ js/jova.js
@@ -4,15 +4,6 @@ function start()
  document.getElementById('img1').style.visibility='visible';
  document.getElementById('img2').style.visibility='visible';
  document.getElementById('img3').style.visibility='visible';
-//document.getElementById('img4').style.visibility='visible';
-// document.getElementById('img5').style.visibility='visible';
-// document.getElementById('img6').style.visibility='visible';
- //document.getElementById('img7').style.visibility='visible';
- // document.getElementById('img8').style.visibility='visible';
- // document.getElementById('img9').style.visibility='visible';
- // document.getElementById('img10').style.visibility='visible';
- // document.getElementById('img11').style.visibility='visible';
-//document.getElementById('img12').style.visibility='visible';
 document.getElementById('p1').style.visibility='visible';
 }
 
