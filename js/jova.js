6d1ac6ee80611faa7bf4350a24fbcdf240924405
AshwinChandlapur <ashwinyga@rocketmail.com>
2016-03-07 01:54:05 +0530

    Vedic Math Using SVG
    The SVG content is displayed as per the instruction Ser.

---
+++ js/jova.js
@@ -0,0 +1,67 @@
+function start()
+{
+
+ document.getElementById('img1').style.visibility='visible';
+ document.getElementById('img2').style.visibility='visible';
+ document.getElementById('img3').style.visibility='visible';
+//document.getElementById('img4').style.visibility='visible';
+// document.getElementById('img5').style.visibility='visible';
+// document.getElementById('img6').style.visibility='visible';
+ //document.getElementById('img7').style.visibility='visible';
+ // document.getElementById('img8').style.visibility='visible';
+ // document.getElementById('img9').style.visibility='visible';
+ // document.getElementById('img10').style.visibility='visible';
+ // document.getElementById('img11').style.visibility='visible';
+//document.getElementById('img12').style.visibility='visible';
+document.getElementById('p1').style.visibility='visible';
+}
+
+function loadb1()
+{
+  document.getElementById('rule1').style.visibility='visible';
+}
+function r1()
+{
+	document.getElementById('p2').style.visibility="visible";
+	document.getElementById('img4').style.visibility='visible';
+document.getElementById('img5').style.visibility='visible';
+document.getElementById('img6').style.visibility='visible';
+ document.getElementById('img7').style.visibility='visible';
+ document.getElementById('img8').style.visibility='visible';
+ 
+}
+
+function loadb2()
+{
+	document.getElementById('rule2').style.visibility='visible';
+}
+function r2()
+{
+	document.getElementById('p3').style.visibility="visible";
+	document.getElementById('img9').style.visibility='visible';
+ document.getElementById('img10').style.visibility='visible';
+}
+
+
+function loadb3()
+{
+	document.getElementById('rule3').style.visibility='visible';
+}
+function r3()
+{
+	document.getElementById('p4').style.visibility="visible";
+	document.getElementById('img11').style.visibility='visible';
+ }
+
+
+function loadb4()
+{
+	document.getElementById('rule4').style.visibility='visible';
+}
+function r4()
+{
+	document.getElementById('p5').style.visibility="visible";
+	document.getElementById('img12').style.visibility='visible';
+ document.getElementById('rule2').style.visibility='visible';
+}
+
